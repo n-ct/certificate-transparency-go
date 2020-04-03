7331cc798eff3b1b603cef3a5c4db918d1e8007b
Zorawar Moolenaar <zorawar87@gmail.com>
2020-04-03 19:18:53 -0400

    fixup

--- trillian/feeder/feeder.go
+++ trillian/feeder/feeder.go
@@ -3,7 +3,7 @@ package feeder
 import (
 	"context"
 	"flag"
-	"fmt"
+  "fmt"
 	"net/http"
 	_ "os"
 	_ "os/signal"
@@ -11,11 +11,11 @@ import (
 	_ "syscall"
 	"time"
 
+  ct "github.com/google/certificate-transparency-go"
 	_ "github.com/coreos/etcd/clientv3"
 	_ "github.com/coreos/etcd/clientv3/naming"
 	"github.com/golang/glog"
 	"github.com/golang/protobuf/ptypes"
-	ct "github.com/google/certificate-transparency-go"
 	"github.com/google/certificate-transparency-go/trillian/ctfe"
 	"github.com/google/certificate-transparency-go/trillian/ctfe/configpb"
 	_ "github.com/google/certificate-transparency-go/trillian/util"
@@ -38,95 +38,95 @@ import (
 )
 
 var (
-	logID      = flag.Int64("log_id", 4963881968625083525, "Trillian LogID to populate.")
-	logConfig  = flag.String("log_config", "./trillian/multilog/demo-script.cfg", "File holding log config in text proto format")
-	rpcBackend = flag.String("log_rpc_server", "13.90.199.163:8090", "Backend specification; comma-separated list or etcd service name (if --etcd_servers specified). If unset backends are specified in config (as a LogMultiConfig proto)")
-	// rpcDeadline        = flag.Duration("rpc_deadline", time.Second*10, "Deadline for backend RPC requests")
-	rpcDeadline    = time.Second * 10
-	getSTHInterval = flag.Duration("get_sth_interval", time.Second*180, "Interval between internal get-sth operations (0 to disable)")
-	httpEndpoint   = flag.String("http_endpoint", "0.0.0.0:6965", "Endpoint for HTTP (host:port)")
-	// maskInternalErrors = flag.Bool("mask_internal_errors", false, "Don't return error strings with Internal Server Error HTTP responses")
-	maskInternalErrors = false
-	quotaRemote        = flag.Bool("quota_remote", true, "Enable requesting of quota for IP address sending incoming requests")
-	quotaIntermediate  = flag.Bool("quota_intermediate", true, "Enable requesting of quota for intermediate certificates in sumbmitted chains")
-	// handlerPrefix      = flag.String("handler_prefix", "", "If set e.g. to '/logs' will prefix all handlers that don't define a custom prefix")
-	handlerPrefix = ""
+			 	logID       = flag.Int64("log_id", 4963881968625083525, "Trillian LogID to populate.")
+        logConfig          = flag.String("log_config", "./trillian/multilog/demo-script.cfg", "File holding log config in text proto format")
+        rpcBackend         = flag.String("log_rpc_server", "13.90.199.163:8090", "Backend specification; comma-separated list or etcd service name (if --etcd_servers specified). If unset backends are specified in config (as a LogMultiConfig proto)")
+        // rpcDeadline        = flag.Duration("rpc_deadline", time.Second*10, "Deadline for backend RPC requests")
+        rpcDeadline        = time.Second*10
+        getSTHInterval     = flag.Duration("get_sth_interval", time.Second*180, "Interval between internal get-sth operations (0 to disable)")
+        httpEndpoint       = flag.String("http_endpoint", "0.0.0.0:6965", "Endpoint for HTTP (host:port)")
+        // maskInternalErrors = flag.Bool("mask_internal_errors", false, "Don't return error strings with Internal Server Error HTTP responses")
+        maskInternalErrors = false
+        quotaRemote        = flag.Bool("quota_remote", true, "Enable requesting of quota for IP address sending incoming requests")
+        quotaIntermediate  = flag.Bool("quota_intermediate", true, "Enable requesting of quota for intermediate certificates in sumbmitted chains")
+        // handlerPrefix      = flag.String("handler_prefix", "", "If set e.g. to '/logs' will prefix all handlers that don't define a custom prefix")
+        handlerPrefix      = ""
 )
 
 const unknownRemoteUser = "UNKNOWN_REMOTE"
 
 type Mouth struct {
-	keepConnAlive *grpc.ClientConn
-	tladmin       trillian.TrillianAdminClient
-	tlclient      trillian.TrillianLogClient
-	ClientToTree  map[string]*trillian.Tree
+  keepConnAlive *grpc.ClientConn
+  tladmin trillian.TrillianAdminClient
+  tlclient trillian.TrillianLogClient
+  ClientToTree map[string]*trillian.Tree
 }
 
 /// Dial Backend
-func InitializeFeeder(ctx context.Context, rpcEndpoint string, logUrls []string) *Mouth {
-	dialOpts := []grpc.DialOption{grpc.WithInsecure(), grpc.WithBlock()}
-	glog.Infof("Dialling backend: /%v/ with opts /%v/", rpcBackend, dialOpts)
-	conn, err := grpc.Dial(rpcEndpoint, dialOpts...)
-	if err != nil {
-		glog.Exitf("Could not dial RPC server: %v: %v", rpcBackend, err)
-	} else {
-		glog.Infof("Successfully dialed Trillian RPC server: %v", rpcBackend)
-	}
-	defer conn.Close()
+func InitializeFeeder(ctx context.Context, rpcEndpoint string, logUrls []string) (*Mouth) {
+  dialOpts := []grpc.DialOption{grpc.WithInsecure(), grpc.WithBlock()}
+  glog.Infof("Dialling backend: /%v/ with opts /%v/", rpcBackend, dialOpts)
+  conn, err := grpc.Dial(rpcEndpoint, dialOpts...)
+  if err != nil {
+    glog.Exitf("Could not dial RPC server: %v: %v", rpcBackend, err)
+  } else {
+    glog.Infof("Successfully dialed Trillian RPC server: %v", rpcBackend)
+  }
+  defer conn.Close()
 
-	mouth := &Mouth{
-		keepConnAlive: conn,
-		tladmin:       trillian.NewTrillianAdminClient(conn),
-		tlclient:      trillian.NewTrillianLogClient(conn),
-		ClientToTree:  make(map[string]*trillian.Tree),
-	}
-	var tree *trillian.Tree
-	for i, v := range logUrls {
-		tree, err = CreateAndInitTree(ctx, mouth)
-		if err != nil {
-			glog.Fatalf("could not create tree(%v|%v): %+v", i, v, err)
-		}
-		mouth.ClientToTree[v] = tree
-		glog.Infof("Created %v of %v trees", i+1, len(logUrls))
-	}
-	return mouth
+  mouth := &Mouth{
+    keepConnAlive: conn,
+    tladmin: trillian.NewTrillianAdminClient(conn),
+    tlclient: trillian.NewTrillianLogClient(conn),
+    ClientToTree: make(map[string]*trillian.Tree),
+  }
+  var tree *trillian.Tree
+  for i, v := range logUrls {
+    tree, err = CreateAndInitTree(ctx, mouth)
+    if err != nil {
+      glog.Fatalf("could not create tree(%v|%v): %+v", i,v, err)
+    }
+    mouth.ClientToTree[v] = tree
+    glog.Infof("Created %v of %v trees", i+1, len(logUrls))
+  }
+  return mouth
 }
 
 func CreateAndInitTree(ctx context.Context, mouth *Mouth) (*trillian.Tree, error) {
-	req := newCreateDefaultTreeRequest()
-	if req == nil {
-		glog.Warningf("CANNOT CREATE NEW TREE REQUEST")
-		return nil, fmt.Errorf("Could not create new tree request")
-	}
+  req := newCreateDefaultTreeRequest()
+  if req == nil {
+    glog.Warningf ("CANNOT CREATE NEW TREE REQUEST")
+    return nil, fmt.Errorf("Could not create new tree request")
+  }
 
-	tree, err := client.CreateAndInitTree(ctx, req, mouth.tladmin, nil, mouth.tlclient)
-	if err != nil {
-		glog.Warningf("CANNOT CREATE AND INIT TREE: %+v", err)
-		return nil, fmt.Errorf("Could not create and init tree: %+v", err)
-	}
+  tree, err := client.CreateAndInitTree(ctx, req, mouth.tladmin, nil, mouth.tlclient)
+  if err != nil {
+    glog.Warningf ("CANNOT CREATE AND INIT TREE: %+v", err)
+    return nil, fmt.Errorf("Could not create and init tree: %+v", err)
+  }
 
-	return tree, nil
+  return tree, nil
 }
 
-func Feed(ctx context.Context, gossipReq ct.GossipExchangeRequest) error {
-	flag.Parse()
+func Feed(ctx context.Context, gossipReq ct.GossipExchangeRequest) (error){
+  flag.Parse()
 
-	tl := &trillian.LogLeaf{LeafValue: []byte("somecustomdata1212121")}
+  tl := &trillian.LogLeaf{LeafValue: []byte("somecustomdata1212121")}
 	q := &trillian.QueueLeafRequest{LogId: 2, Leaf: tl}
 	// r, err := tlclient.QueueLeaf(ctx, q)
 	// if err != nil{
 	// 	glog.Fatalf("Could not queue leaf:\n----- %+v\n------", err)
 	// }
-	glog.Infof("Data \\Was/ QUEUED!: \n%+s\n---------\n%+q\n", q, tl)
+	glog.Infof("Data \\Was/ QUEUED!: \n%+s\n---------\n%+q\n", q,tl)
 
-	return nil
+  return nil
 }
 
 /// adapted from from ./trillian/integration/ct_server/main.go
 func setupAndRegister(
 	ctx context.Context, client trillian.TrillianLogClient, deadline time.Duration,
 	cfg *configpb.LogConfig, mux *http.ServeMux, globalHandlerPrefix string, maskInternalErrors bool,
-) (*ctfe.Instance, error) {
+	) (*ctfe.Instance, error) {
 	vCfg, err := ctfe.ValidateLogConfig(cfg)
 	if err != nil {
 		return nil, err
@@ -178,7 +178,7 @@ func setupAndRegister(
 	return inst, nil
 }
 
-func newCreateDefaultTreeRequest() *trillian.CreateTreeRequest {
+func newCreateDefaultTreeRequest() (*trillian.CreateTreeRequest) {
 	sa := sigpb.DigitallySigned_ECDSA
 	ctr := &trillian.CreateTreeRequest{Tree: &trillian.Tree{
 		TreeState:          trillian.TreeState_ACTIVE,
@@ -187,7 +187,7 @@ func newCreateDefaultTreeRequest() *trillian.CreateTreeRequest {
 		HashAlgorithm:      sigpb.DigitallySigned_SHA256,
 		SignatureAlgorithm: sigpb.DigitallySigned_SignatureAlgorithm(sa),
 		DisplayName:        "Gossiper",
-		Description:        "TestCreated@" + time.Now().String(),
+		Description:        "TestCreated@"+time.Now().String(),
 		MaxRootDuration:    ptypes.DurationProto(0),
 	}}
 	glog.Infof("Creating tree %+v", ctr.Tree)
