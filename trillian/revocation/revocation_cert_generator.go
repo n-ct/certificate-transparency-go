ad3ceadfae7949a1cdda31a7b0abe861dc9fff77
Jeremy Bennett <jeremy.bennett@uconn.edu>
2020-04-06 09:10:42 -0400

    generated certs are added to log

--- trillian/revocation/revocation_cert_generator.go
+++ trillian/revocation/revocation_cert_generator.go
@@ -1,20 +1,32 @@
 package revocation
 
 import (
+  "context"
+  "flag"
+
   "github.com/golang/glog"
   "github.com/google/certificate-transparency-go/trillian/integration"
   "github.com/google/certificate-transparency-go/trillian/ctfe"
 )
 
+var (
+  logConfig = flag.String("log_config", "../integration/demo-script.cfg")
+  httpServers = flag.String("ct_http_servers", "localhost:6965")
+  testDir = flag.String("testdata_dir", "../testdata")
+)
+
 // syntheticly generates certs using testdir and adds to log specified in config
 // Uses multiple functions used in ct_hammer
 // Only intended for a single log to be specified in logConfig
-func GenerateAndAdd(logConfig *string, httpServers *string, testDir *string, numCerts int) error {
+func GenerateAndAdd(numCerts int) error {
 
   cfg, err := ctfe.LogConfigFromFile(*logConfig)
   if err != nil {
     glog.Exitf("Failed to read log config: %v", err)
   }
+
+  ctx, cancel = context.WithCancel(context.Background())
+  defer cancel()
 
   generatorFactory,err := integration.SyntheticGeneratorFactory(*testDir, "")
   if err != nil {
@@ -22,10 +34,10 @@ func GenerateAndAdd(logConfig *string, httpServers *string, testDir *string, num
   }
 
   c := cfg[0] // Using first log only
-//  pool, err := integration.NewRandomPool(*httpServers, c.PublicKey, c.Prefix)
-//  if err != nil {
-//    glog.Exitf("Failed to create client pool: %v", err)
-//  }
+  pool, err := integration.NewRandomPool(*httpServers, c.PublicKey, c.Prefix)
+  if err != nil {
+    glog.Exitf("Failed to create client pool: %v", err)
+  }
 
   generator, err := generatorFactory(c)
   if err != nil {
@@ -39,6 +51,12 @@ func GenerateAndAdd(logConfig *string, httpServers *string, testDir *string, num
       glog.Exitf("failed to make fresh cert: %v", err)
     }
     glog.Infof("i: %d, generated new cert: %v",i,chain)
+
+    sct, err := pool.Next().AddChain(ctx,chain)
+    if err != nil {
+      if err, ok := err.(client.RspError); ok {
+        glog.Errorf("add-chain: error %v HTTP status %d body %s", err.Error(), err.StatusCode, err.Body)
+      }
   }
 
   return nil
